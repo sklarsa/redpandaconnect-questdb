fe5d81d7da4451d8acb27f49158fc7edb32d07e9
Steven Sklar <sklarsa@gmail.com>
2024-08-15 10:20:49 -0400

    Revert "eod commit, not working with todos"
    This reverts commit af231f8dd7564733fe283701274b970eb181441b.

--- questdb/integration_test.go
+++ questdb/integration_test.go
@@ -4,34 +4,55 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path"
 	"strconv"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/jackc/pgconn"
+	qdb "github.com/questdb/go-questdb-client/v3"
 
 	"github.com/ory/dockertest/v3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/redpanda-data/benthos/v4/public/service/integration"
-
-	qdb "github.com/questdb/go-questdb-client/v3"
 )
 
-func TestIntegrationQuestDBBasic(t *testing.T) {
-	integration.CheckSkip(t)
-
+func TestIntegrationQuestDB(t *testing.T) {
 	ctx := context.Background()
-	pool := setupDockertestPool(t)
-	resource := setupQuestDB(t, ctx, pool)
 
+	integration.CheckSkip(t)
 	t.Parallel()
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	pool.MaxWait = time.Minute * 3
+	resource, err := pool.Run("questdb/questdb", "8.0.0", []string{
+		"JAVA_OPTS=-Xms512m -Xmx512m",
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	if err = pool.Retry(func() error {
+		clientConfStr := fmt.Sprintf("http::addr=localhost:%v", resource.GetPort("9000/tcp"))
+		sender, err := qdb.LineSenderFromConf(ctx, clientConfStr)
+		if err != nil {
+			return err
+		}
+		defer sender.Close(ctx)
+		err = sender.Table("ping").Int64Column("test", 42).AtNow(ctx)
+		if err != nil {
+			return err
+		}
+		return sender.Flush(ctx)
+	}); err != nil {
+		t.Fatalf("Could not connect to docker resource: %s", err)
+	}
+
+	_ = resource.Expire(900)
 
 	template := `
 output:
@@ -69,115 +90,4 @@ output:
 		t, template,
 		integration.StreamTestOptPort(resource.GetPort("9000/tcp")),
 	)
-}
-
-func TestIntegrationQuestDBConfigOptions(t *testing.T) {
-	testCases := []struct {
-		name      string
-		qdbConfig string
-		input     []string
-		validate  func(context.Context, *pgconn.PgConn)
-	}{}
-
-	pool := setupDockertestPool(t)
-	ctx := context.Background()
-
-	t.Parallel()
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			qdb := setupQuestDB(t, ctx, pool)
-			// todo: pass qdb port to qdbConfig
-			benthos := setupBenthos(t, pool, tc.qdbConfig)
-
-			for _, line := range tc.input {
-				resp, err := http.Post(
-					fmt.Sprintf("http://localhost:%s", benthos.GetPort("4195/tcp")),
-					"application/json",
-					strings.NewReader(line),
-				)
-
-				require.NoError(t, err)
-				_, err = io.ReadAll(resp.Body)
-				require.NoError(t, err)
-				resp.Body.Close()
-
-			}
-
-			pgConn, err := pgconn.Connect(ctx, fmt.Sprintf("postgresql://admin:quest@localhost:%v", qdb.GetPort("8812/tcp")))
-			require.NoError(t, err)
-			defer pgConn.Close(ctx)
-
-			tc.validate(ctx, pgConn)
-
-		})
-	}
-
-}
-
-func setupDockertestPool(t *testing.T) *dockertest.Pool {
-	pool, err := dockertest.NewPool("")
-	require.NoError(t, err)
-	pool.MaxWait = time.Minute * 3
-	return pool
-
-}
-
-func setupQuestDB(t *testing.T, ctx context.Context, pool *dockertest.Pool) *dockertest.Resource {
-
-	resource, err := pool.Run("questdb/questdb", "8.0.0", []string{
-		"JAVA_OPTS=-Xms512m -Xmx512m",
-	})
-
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		assert.NoError(t, pool.Purge(resource))
-	})
-
-	if err = pool.Retry(func() error {
-		clientConfStr := fmt.Sprintf("http::addr=localhost:%v", resource.GetPort("9000/tcp"))
-		sender, err := qdb.LineSenderFromConf(ctx, clientConfStr)
-		if err != nil {
-			return err
-		}
-		defer sender.Close(ctx)
-		err = sender.Table("ping").Int64Column("test", 42).AtNow(ctx)
-		if err != nil {
-			return err
-		}
-		return sender.Flush(ctx)
-	}); err != nil {
-		t.Fatalf("Could not connect to docker resource: %s", err)
-	}
-
-	_ = resource.Expire(900)
-
-	return resource
-}
-
-func setupBenthos(t *testing.T, pool *dockertest.Pool, qdbConf string) *dockertest.Resource {
-	tmp := t.TempDir()
-
-	// todo: construct basic config (http input) and inject output config with qdbconf
-
-	require.NoError(t, os.WriteFile(path.Join(tmp, "config.yaml"), []byte(conf), 0444))
-
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "jeffail/benthos",
-		Tag:        "4.27",
-		Mounts: []string{
-			path.Join(tmp, "config.yaml", "/benthos.yaml"),
-		},
-	})
-
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		assert.NoError(t, pool.Purge(resource))
-	})
-
-	// todo: wait until container http server is listening...
-
-	_ = resource.Expire(900)
-
-	return resource
 }
